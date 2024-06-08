8084eb42c4e34da3b75a2b52bff30d9ab2147a96
Mehrdadsh7 <mehrdad.shahrbaz09@gmail.com>
2024-06-08 09:52:10 +0330

    Fix caching issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,6 +1,6 @@
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
-import { IssueStatusBadge , Link  } from '@/app/components'
+import { IssueStatusBadge, Link } from "@/app/components";
 
 import IssueActions from "./IssuesActions";
 
@@ -44,5 +44,7 @@ const IssuesPage = async () => {
     </div>
   );
 };
+
+
 
 export default IssuesPage;
