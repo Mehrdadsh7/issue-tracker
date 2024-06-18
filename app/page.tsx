741cfdf93840dcf4bc1cbbe676509586506f7b0c
Mehrdadsh7 <mehrdad.shahrbaz09@gmail.com>
2024-06-18 10:18:06 +0330

    import fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 import prisma from "@/prisma/client";
-import Pagination from "./components/Pagination";
+import { Flex, Grid } from "@radix-ui/themes";
+import IssueChart from "./IssueChart";
 import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
-import IssueChart from "./IssueChart";
-import { Flex, Grid } from "@radix-ui/themes";
 
 export default async function Home() {
  const open = await prisma.issue.count({ where: {status:'OPEN' }})
